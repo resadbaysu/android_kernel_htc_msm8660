3bd9823530b6290ad4b1386bb01b1dea8525419f
Claude Oliver <oliver.claude@gmail.com>
2012-09-17 11:08:30 +0200

    SWEEP2WAKE: Remove debug information

--- drivers/input/touchscreen/atmel.c
+++ drivers/input/touchscreen/atmel.c
@@ -1443,6 +1443,7 @@ static void multi_input_report(struct atmel_ts_data *ts)
 #ifdef CONFIG_TOUCHSCREEN_ATMEL_SWEEP2WAKE
 	int prevx = 0, nextx = 0;
 #endif 
+
 	for (loop_i = 0; loop_i < ts->finger_support; loop_i++) {
 		if (ts->finger_pressed & BIT(loop_i)) {
 			if (ts->id->version >= 0x15) {
@@ -1458,9 +1459,6 @@ static void multi_input_report(struct atmel_ts_data *ts)
 					1, (ts->finger_count == ++finger_report));
 			}
 #ifdef CONFIG_TOUCHSCREEN_ATMEL_SWEEP2WAKE
-			//debug for x dimension
-			printk(KERN_INFO "[sweep2wake_debug]: finger at x = %d\n", ts->finger_data[loop_i].x);
-
 			//left -> right
 			if ((s2w_switch > 0) && (scr_suspended == true) && (ts->finger_count == 1)) {
 				prevx = s2w_startbutton - 100; //0;
