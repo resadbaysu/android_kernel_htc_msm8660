adee9a9a23311181e8a9e5ee8b54c9249cfc71c6
Claude Oliver <oliver.claude@gmail.com>
2012-10-30 10:40:32 +0200

    MPDEC: Implemented advanced scroff freq in supporting files for msm_mpdecision.c, and fix typing mistakes

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -26,6 +26,8 @@
 
 #ifdef CONFIG_MSM_MPDEC
 extern uint32_t acpu_check_khz_value(unsigned long khz);
+
+ssize_t set_scaling_max(unsigned int new_max, int cpu);
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_GPU_CONTROL
@@ -334,6 +336,7 @@ static inline unsigned int cpufreq_get(unsigned int cpu)
 /* query the last known CPU freq (in kHz). If zero, cpufreq couldn't detect it */
 #ifdef CONFIG_CPU_FREQ
 unsigned int cpufreq_quick_get(unsigned int cpu);
+unsigned int cpufreq_quick_get_max(unsigned int cpu);
 #else
 static inline unsigned int cpufreq_quick_get(unsigned int cpu)
 {
