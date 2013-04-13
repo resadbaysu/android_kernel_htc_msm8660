fb6ac4bdd80dbebd067f1370b8641119557fb811
MikeC84 <mikec4891@gmail.com>
2013-04-13 15:47:32 -0700

    acpu/avs: push max voltage

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -68,8 +68,8 @@
 #define L_VAL_SCPLL_CAL_MAX	0x24 /* = 1944 MHz with 27MHz source */ 
 
 #define MIN_VDD_SC      	 700000 /* uV */ 
-#define MAX_VDD_SC		1350000 /* uV */
-#define MAX_VDD_MEM		1350000 /* uV */
+#define MAX_VDD_SC		1450000 /* uV */
+#define MAX_VDD_MEM		1450000 /* uV */
 #define MAX_VDD_DIG		1300000 /* uV */
 #define MAX_AXI			 310500 /* KHz */
 #define SCPLL_LOW_VDD_FMAX	 594000 /* KHz */
