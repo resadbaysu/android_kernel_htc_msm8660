e4418349ddde1d7fad71401bb5da1949aeb628f6
MikeC84 <mikec4891@gmail.com>
2013-04-14 00:22:36 -0700

    board: fix undefined reference warning

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -2106,7 +2106,7 @@ struct platform_device msm_device_gadget_peripheral = {
 		.coherent_dma_mask	= 0xffffffffULL,
 	},
 };
-#ifdef CONFIG_USB_EHCI_MSM_72K
+
 static struct resource resources_hsusb_host[] = {
 	{
 		.start	= 0x12500000,
@@ -2130,7 +2130,7 @@ struct platform_device msm_device_hsusb_host = {
 		.coherent_dma_mask	= 0xffffffffULL,
 	},
 };
-
+#ifdef CONFIG_USB_EHCI_MSM_72K
 static struct platform_device *msm_host_devices[] = {
 	&msm_device_hsusb_host,
 };
