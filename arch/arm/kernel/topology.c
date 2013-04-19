d14539a8a90ab3506171ef4906cd253b9abe6afb
MikeC84 <mikec4891@gmail.com>
2013-04-18 23:45:38 -0700

    Revert "ARM: cpu topology: Modify cpu_power according to sched_mc level"
    This reverts commit 34e71ee0a2a7a7f8e23e7403714cafdb18f5da3d.

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -19,7 +19,6 @@
 #include <linux/nodemask.h>
 #include <linux/sched.h>
 #include <linux/cpumask.h>
-#include <linux/cpuset.h>
 
 #include <asm/cputype.h>
 #include <asm/topology.h>
@@ -50,43 +49,6 @@ struct cputopo_arm cpu_topology[NR_CPUS];
  */
 
 unsigned int advanced_topology = 1;
-
-static void normal_cpu_topology_mask(void);
-static void (*set_cpu_topology_mask)(void) = normal_cpu_topology_mask;
-
-/* This table sets the cpu_power scale of a cpu according to the sched_mc mode.
- * The content of this table could be SoC specific so we should add a method to
- * overwrite this default table.
- * TODO: Study how to use DT for setting this table
- */
-#define ARM_CORTEX_A9_DEFAULT_SCALE 0
-#define ARM_CORTEX_A9_POWER_SCALE 1
-/* This table list all possible cpu power configuration */
-unsigned int table_config[2] = {
-	1024,
-	4096
-};
-
-static void set_power_scale(unsigned int cpu, unsigned int idx)
-{
-	per_cpu(cpu_scale, cpu) = table_config[idx];
-}
-
-static int init_cpu_power_scale(void)
-{
-	/* Do we need to change default config */
-	advanced_topology = 1;
-
-	/* force topology update */
-	arch_update_cpu_topology();
-
-	/* Force a cpu topology update */
-	rebuild_sched_domains();
-
-	return 0;
-}
-
-core_initcall(init_cpu_power_scale);
 
 /*
  * default topology function
@@ -147,7 +109,6 @@ static void normal_cpu_topology_mask(void)
 
 	for_each_possible_cpu(cpuid) {
 		default_cpu_topology_mask(cpuid);
-		set_power_scale(cpuid, ARM_CORTEX_A9_DEFAULT_SCALE);
 	}
 	smp_wmb();
 }
@@ -158,6 +119,7 @@ static void normal_cpu_topology_mask(void)
  */
 static void power_cpu_topology_mask_CA9(void)
 {
+
 	unsigned int cpuid, cpu;
 
 	for_each_possible_cpu(cpuid) {
@@ -182,7 +144,6 @@ static void power_cpu_topology_mask_CA9(void)
 				}
 			}
 		}
-		set_power_scale(cpuid, ARM_CORTEX_A9_POWER_SCALE);
 	}
 	smp_wmb();
 }
@@ -297,7 +258,7 @@ int arch_update_cpu_topology(void)
 	/* set topology policy */
 	update_cpu_topology_policy();
 
-	/* set topology mask and power */
+	/* set topology mask*/
 	(*set_cpu_topology_mask)();
 
 	return 1;
